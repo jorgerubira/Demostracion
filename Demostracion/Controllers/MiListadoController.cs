ab89310ee76c2e0dfe7553e524d4273e300bc20e
Jorge Rubira Santos <jorgesub@gmail.com>
2021-05-03 09:32:09 +0200

    Subir

---
+++ Demostracion/Controllers/MiListadoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demostracion.Models;
+using Demostracion.ViewData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demostracion.Controllers
+{
+    public class MiListadoController : Controller
+    {
+
+        private readonly Contexto _context;
+        private readonly ServicioSQL _sql;
+
+        public MiListadoController(Contexto context, ServicioSQL sql)
+        {
+            _context = context;
+            _sql = sql;
+        }
+
+        public IActionResult Index()
+        {
+
+            string sql= " SELECT h.id, h.hospital, ifnull(MAX(camas_uci_ocupadas),0) " +
+                        " FROM cov_h_camas c "+
+                        " INNER JOIN cov_m_hospitales h ON(c.id_hospital= h.id) " +
+                        " GROUP BY h.id, h.hospital ";
+
+            List<MiListadoViewData> lista=
+                    _sql.EjecutarSQL<MiListadoViewData>(
+                        _context, sql,
+                        x => new MiListadoViewData()
+                        {
+                            id = x.GetInt32(0),
+                            hospital = x.GetString(1),
+                            camas = x.GetInt32(2)
+                        }
+                    ); 
+
+            return View(lista);
+        }
+    }
+}
---
+++ Demostracion/ViewData/MiListadoViewData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demostracion.ViewData
+{
+    public class MiListadoViewData
+    {
+        public int id { get; set; }
+        public string hospital { get; set; }
+        public int? camas { get; set; }
+    }
+}
